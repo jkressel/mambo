ae9c0ae059ef572b64f1d4186345188163b3992c
John Kressel <alistair@kressel.eu>
2021-10-29 16:22:51 +0100

    Unconditional branch linking

--- arch/riscv/dispatcher_riscv.c
+++ arch/riscv/dispatcher_riscv.c
@@ -146,7 +146,7 @@ void dispatcher_riscv(dbm_thread *thread_data, uint32_t source_index, branch_typ
     case jal_riscv:
       uint16_t *branch_addr = thread_data->code_cache_meta[source_index].exit_branch_addr;
       intptr_t offset = (block_address+6) - (intptr_t)branch_addr;
-      if (abs(offset) <= 0xFFFFF) {
+      if (offset <= 0xFFFFF && offset >= -0xFFFFF) {
         riscv_cc_jump(thread_data, branch_addr, block_address+6);
         __clear_cache((void *)branch_addr, (void *)branch_addr+2);
       } else {
