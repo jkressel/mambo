25ae13f27da070061896830f40fc9609cb7c3cfb
John Kressel <alistair@kressel.eu>
2021-11-01 21:24:45 +0000

    Merge

--- plugins/cnd_branch_print.c
+++ plugins/cnd_branch_print.c
@@ -2,24 +2,12 @@
   This file is part of MAMBO, a low-overhead dynamic binary modification tool:
       https://github.com/beehive-lab/mambo
 
-<<<<<<< HEAD
-  Copyright 2013-2016 Cosmin Gorgovan <cosmin at linux-geek dot org>
-  Copyright 2017 The University of Manchester
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-=======
   Copyright 2021 The University of Manchester
   
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
       http://www.apache.org/licenses/LICENSE-2.0
->>>>>>> riscv-plugins
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
